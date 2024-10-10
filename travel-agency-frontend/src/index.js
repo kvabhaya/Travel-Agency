93f0562a6fa622f64320a7820232dd5e399b7c96
Supuni-Punsarani <114279717+Supuni-Punsarani@users.noreply.github.com>
2024-10-11 00:47:29 +0530

    finish recommendation design on home page

--- travel-agency-frontend/src/index.js
+++ travel-agency-frontend/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import App from './App';
 import './index.css';
 
-
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter> {/* Wrap App with BrowserRouter */}
+            <App />
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
-
